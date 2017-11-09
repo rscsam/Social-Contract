87b35f56f4889f9045c1ca50a14887dad7de9fc8
rscsam <sam.costley@yahoo.com>
2017-11-09 15:23:19 -0500

    Added navigation for all of Tutorial, as well as the Main screens including Home, Discover, and Grow. Updated Readme to include some Conventions

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Social-Contract
+# Social Contract
+
+This is the Android version of an application that integrates 
+many types of social media into one application that then
+allows users to gain following and follow new people
+utilizing a centralized virtual currency called "coins."
+<h2>Coding Conventions</h2>
+<h3>Activities and Fragments</h3>
+In general, if two screens share common elements fixed to the same
+part of the screen, they should be coded as Fragments
+that are managed by the same activity.
+
+In order to maintain a level of reusability and easy refactoring,
+all `Fragment`s will interact with their   `Activity` through
+interfaces defined by the `Fragment` and implemented by the
+`Activity`.  Many examples of this pattern are already being
+implemented within the application.
+
+<h3>Naming and Style Conventions</h3>
+<h4>Fragments</h4>
+Listener interfaces defined within fragments should follow the
+following naming convention:
+
+[Fragment Name]FListener
+
+For example, if you have a `Fragment` called `HomeFragment`,
+you should name your interface `HomeFListener`
+
+<h4>Method Names</h4>
+Method names should always follow typical Java conventions
+(camelcase, etc.).  Additionally, all onClick methods
+should follow the following naming convention:
+
+onClick[View Name]
+
+For example, if you have a button that is called `submitButton`,
+your should name your onClick method `onClickSubmit`.
+
+If the onClick is declared inside of a `Fragment` initially,
+it should follow the following naming convention:
+
+onClick[Fragment Name][View Name]
+
+For example, if you have `submitButton`'s onClick declared
+inside of the `HomeFragment`, the corresponding onClick
+method should be called `onClickHomeSubmit`
--- app/src/main/java/jd7337/socialcontract/controller/activity/TutorialActivity.java
+++ app/src/main/java/jd7337/socialcontract/controller/activity/TutorialActivity.java
@@ -1,19 +1,21 @@
 package jd7337.socialcontract.controller.activity;
 
-import android.net.Uri;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import jd7337.socialcontract.R;
 import jd7337.socialcontract.controller.fragment.EditInterestProfileFragment;
 import jd7337.socialcontract.controller.fragment.EditInterestProfilePromptFragment;
 import jd7337.socialcontract.controller.fragment.InitialConnectAccountFragment;
 
+
 public class TutorialActivity extends AppCompatActivity
-        implements InitialConnectAccountFragment.OnInitialConnectAccountInteractionListener,
-        EditInterestProfilePromptFragment.OnFragmentInteractionListener {
+        implements InitialConnectAccountFragment.InitialConnectAccountFListener,
+        EditInterestProfilePromptFragment.EditInterestProfilePromptFListener,
+        EditInterestProfileFragment.EditInterestProfileFListener {
 
     private InitialConnectAccountFragment icaFragment;
     private EditInterestProfilePromptFragment eippFragment;
@@ -33,22 +35,46 @@ public class TutorialActivity extends AppCompatActivity
         eippFragment = new EditInterestProfilePromptFragment();
         eipFragment = new EditInterestProfileFragment();
 
-        // add the first
+        // show initial fragment
         getSupportFragmentManager().beginTransaction().add(R.id.tutorial_activity,
                 icaFragment).commit();
     }
 
     @Override
     public void onClickICAFConnectAccount() {
-        // replace the current fragment with eippFragment
+        showFragment(R.id.tutorial_activity, eippFragment);
+    }
+
+    @Override
+    public void onClickEIPPEditProfile() {
+        showFragment(R.id.tutorial_activity, eipFragment);
+    }
+
+    @Override
+    public void onClickEIPPSkip() {
+        startMainActivity();
+    }
+
+    @Override
+    public void onClickEIPSubmit() {
+        startMainActivity();
+    }
+
+    private void startMainActivity() {
+        Intent startMain = new Intent(this, MainActivity.class);
+        startActivity(startMain);
+    }
+
+    private void showFragment(int viewId, Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.tutorial_activity, eippFragment);
+        transaction.replace(viewId, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
+    private void showFragmentNoBackStack(int viewId, Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(viewId, fragment);
+        transaction.commit();
     }
 }
