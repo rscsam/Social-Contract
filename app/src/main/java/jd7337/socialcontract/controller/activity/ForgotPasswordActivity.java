99aeac0ef4b7d4e0ac383f51ea4bae07018e0de4
rscsam <sam.costley@yahoo.com>
2017-11-08 19:28:29 -0500

    added fragments and activities for most screens

---
+++ app/src/main/java/jd7337/socialcontract/controller/activity/ForgotPasswordActivity.java
@@ -0,0 +1,15 @@
+package jd7337.socialcontract.controller.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jd7337.socialcontract.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
