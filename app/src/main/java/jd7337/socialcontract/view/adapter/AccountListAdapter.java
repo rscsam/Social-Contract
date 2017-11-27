2eff9d1c841d988d56800d53517960281b597771
rscsam <sam.costley@yahoo.com>
2017-11-26 21:34:35 -0500

    Additional settings view automatically populates after an account is clicked.  It will not go away on second tap and the checkboxes do not work.

--- app/src/main/java/jd7337/socialcontract/view/adapter/AccountListAdapter.java
+++ app/src/main/java/jd7337/socialcontract/view/adapter/AccountListAdapter.java
@@ -7,35 +7,54 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import jd7337.socialcontract.R;
+import jd7337.socialcontract.controller.activity.MainActivity;
 import jd7337.socialcontract.view.holder.AccountListItem;
 
 public class AccountListAdapter extends ArrayAdapter<AccountListItem> {
 
-    private final Activity context;
+    private final MainActivity context;
     private final AccountListItem[] accounts;
 
     public AccountListAdapter(Activity context, AccountListItem[] accounts) {
         super(context, R.layout.account_select_item, accounts);
-        this.context = context;
+        this.context = (MainActivity) context;
         this.accounts = accounts;
     }
 
     @NonNull
     public View getView(int position, View view, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
-        View row = inflater.inflate(R.layout.account_select_item, null, false);
+        LinearLayout row = (LinearLayout)
+                inflater.inflate(R.layout.account_select_item, null, false);
+        final LinearLayout additionalSettingsLl =
+                (LinearLayout) inflater.inflate(R.layout.discover_settings_account_setting,
+                        parent, false);
+        Button goButton = additionalSettingsLl.findViewById(R.id.discover_go_button);
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("something happened");
+                context.onClickDiscoverSettingsGo();
+            }
+        });
 
         ImageView profilePic = row.findViewById(R.id.profile_picture);
         TextView userName = row.findViewById(R.id.account_username);
         ImageView smPlatform = row.findViewById(R.id.social_media_platform_picture);
 
-        profilePic.setImageResource(accounts[position].getProfilePicId());
-        userName.setText(accounts[position].getProfileName());
-        smPlatform.setImageResource(accounts[position].getSmTypePicId());
+        AccountListItem account = accounts[position];
+        profilePic.setImageResource(account.getProfilePicId());
+        userName.setText(account.getProfileName());
+        smPlatform.setImageResource(account.getSmTypePicId());
+        if (account.isShowingExtraSettings()) {
+            row.addView(additionalSettingsLl);
+        }
 
         return row;
     }
