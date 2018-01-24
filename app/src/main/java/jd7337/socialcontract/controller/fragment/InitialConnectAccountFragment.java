298843458d30bd2c0a400fa84bc9a223b595e38a
bbbzhai <movingbob@hotmail.com>
2018-01-24 15:22:44 -0500

    80% finished with the Facebook authentication

--- app/src/main/java/jd7337/socialcontract/controller/fragment/InitialConnectAccountFragment.java
+++ app/src/main/java/jd7337/socialcontract/controller/fragment/InitialConnectAccountFragment.java
@@ -1,16 +1,26 @@
 package jd7337.socialcontract.controller.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import jd7337.socialcontract.R;
 
 public class InitialConnectAccountFragment extends Fragment {
     private InitialConnectAccountFListener mListener;
+    private CallbackManager callbackManager;
+    private LoginButton fbLoginButton;
 
     public InitialConnectAccountFragment() {
         // Required empty public constructor
@@ -35,8 +45,42 @@ public class InitialConnectAccountFragment extends Fragment {
                     }
                 }
         );
+        callbackManager = CallbackManager.Factory.create();
+        fbLoginButton = (LoginButton) view.findViewById(R.id.login_button);
+        fbLoginButton.setFragment(this);
+        callbackManager = CallbackManager.Factory.create();
+        //callback registration
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast toast = Toast.makeText(getActivity(),"Logged In", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Toast toast = Toast.makeText(getActivity(), "Cancelled", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+
+            }
+
+
+        });
+
         return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 
     @Override
     public void onAttach(Context context) {
