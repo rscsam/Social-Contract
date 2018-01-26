96862e7e0b20bb9cf16d63a6b3557e12d3f9ba81
akhosravi7 <alikhosravi1000@gmail.com>
2018-01-26 11:20:23 -0500

    ali's branch, instagram api inclusion

--- app/src/main/java/jd7337/socialcontract/controller/activity/MainActivity.java
+++ app/src/main/java/jd7337/socialcontract/controller/activity/MainActivity.java
@@ -11,9 +11,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import jd7337.socialcontract.R;
+import jd7337.socialcontract.controller.dialog.AuthenticationDialog;
 import jd7337.socialcontract.controller.fragment.AccountManagementFragment;
 import jd7337.socialcontract.controller.fragment.AccountSelectFragment;
 import jd7337.socialcontract.controller.fragment.ConfirmPurchaseDialogFragment;
@@ -23,6 +25,7 @@ import jd7337.socialcontract.controller.fragment.EditInterestProfileFragment;
 import jd7337.socialcontract.controller.fragment.GrowFragment;
 import jd7337.socialcontract.controller.fragment.HomeFragment;
 import jd7337.socialcontract.controller.fragment.ProfileFragment;
+import jd7337.socialcontract.controller.listener.AuthenticationListener;
 
 public class MainActivity extends AppCompatActivity implements
     HomeFragment.HomeFListener, DiscoverSettingsFragment.DiscoverSettingsFListener,
@@ -46,6 +49,9 @@ public class MainActivity extends AppCompatActivity implements
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
     private NavigationView mDrawerList;
+
+    private AuthenticationDialog auth_dialog;
+    private Button btn_get_access_token;
 
     private int numCoins;
 
@@ -236,4 +242,33 @@ public class MainActivity extends AppCompatActivity implements
         String newCoinNumStr = newNumber + "";
         coinTV.setText(newCoinNumStr);
     }
+
+    /*
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_get_access_token = (Button) findViewById(R.id.btn_get_access_token);
+
+        btn_get_access_token.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth_dialog = new AuthenticationDialog(MainActivity.this, MainActivity.this);
+                auth_dialog.setCancelable(true);
+                auth_dialog.show();
+            }
+        });
+    }
+
+    public void onCodeReceived(String access_token) {
+        if (access_token == null) {
+            auth_dialog.dismiss();
+        }
+
+        Intent i = new Intent(MainActivity.this, FeedActivity.class);
+        i.putExtra("access_token", access_token);
+        startActivity(i);
+
+    }
+    */
 }
