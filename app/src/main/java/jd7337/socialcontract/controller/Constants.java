3630a2ba1b577dab4d38cfe1b5dd1b392d674a0b
akhosravi7 <alikhosravi1000@gmail.com>
2018-01-26 12:55:36 -0500

    ali's branch, instagram api inclusion

--- app/src/main/java/jd7337/socialcontract/controller/Constants.java
+++ app/src/main/java/jd7337/socialcontract/controller/Constants.java
@@ -7,5 +7,5 @@ package jd7337.socialcontract.controller;
 public final class Constants {
     public static final String BASE_URL = "https://api.instagram.com/";
     public static final String CLIENT_ID = "56aabe5bc7de4f6fa4cfe9b34e683eda";
-    public static final String REDIRECT_URI = "https://instagram.com/";
+    public static final String REDIRECT_URI = "http://ec2-user@ec2-18-216-24-51.us-east-2.compute.amazonaws.com";
 }
