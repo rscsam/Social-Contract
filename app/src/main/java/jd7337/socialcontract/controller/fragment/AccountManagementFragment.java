f3d0ad1871e6dec217370f27b5cdb9334517b06a
bbbzhai <movingbob@hotmail.com>
2018-02-16 17:03:56 -0500

    Merge branches 'accountmanagement' and 'ryanAccountManagement' of https://github.com/rscsam/Social-Contract into accountmanagement
    # Conflicts:
    #	app/src/main/java/jd7337/socialcontract/controller/fragment/AccountManagementFragment.java

--- app/src/main/java/jd7337/socialcontract/controller/fragment/AccountManagementFragment.java
+++ app/src/main/java/jd7337/socialcontract/controller/fragment/AccountManagementFragment.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -150,6 +151,11 @@ public class AccountManagementFragment extends Fragment {
 
         mContext = getActivity();
         final View view = inflater.inflate(R.layout.fragment_account_management, container, false);
+
+
+
+        LinearLayout twLayout = view.findViewById(R.id.twLinearLayout);
+
 
         userID = mListener.getSocialContractId();
         System.out.println(userID);
