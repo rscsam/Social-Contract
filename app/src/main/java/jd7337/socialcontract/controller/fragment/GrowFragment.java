c2ff4688c8761a24e936f5b847f485ae2d416d22
SethMTriplett <Triplett.seth@gmail.com>
2018-02-27 23:07:47 -0500

    Grow request UI.

--- app/src/main/java/jd7337/socialcontract/controller/fragment/GrowFragment.java
+++ app/src/main/java/jd7337/socialcontract/controller/fragment/GrowFragment.java
@@ -3,8 +3,6 @@ package jd7337.socialcontract.controller.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.constraint.ConstraintSet;
-import android.support.constraint.ConstraintLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,128 +41,161 @@ public class GrowFragment extends Fragment {
         int typeOrdinal = getArguments().getInt("typeInt");
         accountType = SocialMediaAccount.AccountType.values()[typeOrdinal];
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_grow, container, false);
-        View connectAccountButton = view.findViewById(R.id.purchase_button);
-        View facebookCl = view.findViewById(R.id.facebook_cl);
-        // Get all the facebook views
-        ArrayList<View> facebookViews = getAllViewChildren(facebookCl);
-
-        View twitterCl = view.findViewById(R.id.twitter_cl);
-        // Get all the twitter views
-        ArrayList<View> twitterViews = getAllViewChildren(facebookCl);
-
-        View instagramCl = view.findViewById(R.id.instagram_cl);
-        // Get all the instagram views
-        ArrayList<View> instagramViews = getAllViewChildren(facebookCl);
-
-        quantityET = view.findViewById(R.id.quantityET);
-        ConstraintSet constraintSet = new ConstraintSet();
-        ConstraintLayout growLayout = view.findViewById(R.id.grow_cl);
-        constraintSet.clone(growLayout);
+        View view;
         switch (accountType) {
             case FACEBOOK:
-                // Find all the facebook elements and make them visible
-                for (View v : facebookViews) {
-                    v.setVisibility(View.VISIBLE);
-                }
-                // Find all the twitter elements and hide them
-                for (View v : twitterViews) {
-                    v.setVisibility(View.GONE);
-                }
-                // Find all the instagram elements and hide them
-                for (View v : instagramViews) {
-                    v.setVisibility(View.GONE);
-                }
+                view = inflater.inflate(R.layout.fragment_facebook_grow, container, false);
                 interactionTypesRG = view.findViewById(R.id.facebook_interaction_type_rg);
-                constraintSet.connect(quantityET.getId(), ConstraintSet.TOP, facebookCl.getId(), ConstraintSet.BOTTOM);
                 break;
             case TWITTER:
-                // Find all the facebook elements and make them visible
-                for (View v : facebookViews) {
-                    v.setVisibility(View.GONE);
-                }
-                // Find all the twitter elements and hide them
-                for (View v : twitterViews) {
-                    v.setVisibility(View.VISIBLE);
-                }
-                // Find all the instagram elements and hide them
-                for (View v : instagramViews) {
-                    v.setVisibility(View.GONE);
-                }
+                view = inflater.inflate(R.layout.fragment_twitter_grow, container, false);
                 interactionTypesRG = view.findViewById(R.id.twitter_interaction_type_rg);
-                constraintSet.connect(quantityET.getId(), ConstraintSet.TOP, twitterCl.getId(), ConstraintSet.BOTTOM);
                 break;
             case INSTAGRAM:
-                // Find all the facebook elements and make them visible
-                for (View v : facebookViews) {
-                    v.setVisibility(View.GONE);
-                }
-                // Find all the twitter elements and hide them
-                for (View v : twitterViews) {
-                    v.setVisibility(View.GONE);
-                }
-                // Find all the instagram elements and hide them
-                for (View v : instagramViews) {
-                    v.setVisibility(View.VISIBLE);
-                }
+                view = inflater.inflate(R.layout.fragment_instagram_grow, container, false);
                 interactionTypesRG = view.findViewById(R.id.instagram_interaction_type_rg);
-                constraintSet.connect(quantityET.getId(), ConstraintSet.TOP, instagramCl.getId(), ConstraintSet.BOTTOM);
+                break;
+            // If for some reason the type is different, inflate the facebook view instead.
+            default:
+                view = inflater.inflate(R.layout.fragment_facebook_grow, container, false);
+                break;
+        }
+        View connectAccountButton = view.findViewById(R.id.purchase_button);
+        quantityET = view.findViewById(R.id.quantityET);
+        switch (accountType) {
+            case FACEBOOK:
+                connectAccountButton.setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                int quantity;
+                                try {
+                                    quantity = Integer.parseInt(quantityET.getText().toString());
+                                } catch (NumberFormatException e) {
+                                    Toast.makeText(getContext(), "Please choose a quantity.",
+                                            Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+
+                                int selectId = interactionTypesRG.getCheckedRadioButtonId();
+                                RadioButton selected = interactionTypesRG.findViewById(selectId);
+                                if (selected != null) {
+                                    String type = selected.getText().toString();
+                                    type = type.substring(0, type.length() - 1);
+                                    int price;
+                                    switch (type) {
+                                        case "Like":
+                                            price = 1;
+                                            break;
+                                        case "Share":
+                                            price = 5;
+                                            break;
+                                        default:
+                                            price = 10;
+                                            break;
+                                    }
+                                    mListener.onClickGrowPurchase(quantity, type, price);
+                                } else {
+                                    Toast.makeText(getContext(), "Please choose an interaction.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                );
+                break;
+            case TWITTER:
+                System.out.println("Twittered");
+                connectAccountButton.setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                int quantity;
+                                try {
+                                    quantity = Integer.parseInt(quantityET.getText().toString());
+                                } catch (NumberFormatException e) {
+                                    Toast.makeText(getContext(), "Please choose a quantity.",
+                                            Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+
+                                System.out.println("iT: " + interactionTypesRG);
+                                int selectId = interactionTypesRG.getCheckedRadioButtonId();
+                                System.out.println("sI: " + selectId);
+                                RadioButton selected = interactionTypesRG.findViewById(selectId);
+                                if (selected != null) {
+                                    String type = selected.getText().toString();
+                                    type = type.substring(0, type.length() - 1);
+                                    int price;
+                                    switch (type) {
+                                        case "Like":
+                                            price = 1;
+                                            break;
+                                        case "Share":
+                                            price = 5;
+                                            break;
+                                        case "Follow":
+                                            price = 10;
+                                            break;
+                                        default:
+                                            price = 10;
+                                            break;
+                                    }
+                                    mListener.onClickGrowPurchase(quantity, type, price);
+                                } else {
+                                    Toast.makeText(getContext(), "Please choose an interaction.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                );
+                break;
+            case INSTAGRAM:
+                connectAccountButton.setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                int quantity;
+                                try {
+                                    quantity = Integer.parseInt(quantityET.getText().toString());
+                                } catch (NumberFormatException e) {
+                                    Toast.makeText(getContext(), "Please choose a quantity.",
+                                            Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+
+                                int selectId = interactionTypesRG.getCheckedRadioButtonId();
+                                RadioButton selected = interactionTypesRG.findViewById(selectId);
+                                if (selected != null) {
+                                    String type = selected.getText().toString();
+                                    type = type.substring(0, type.length() - 1);
+                                    int price;
+                                    switch (type) {
+                                        case "Like":
+                                            price = 1;
+                                            break;
+                                        case "Share":
+                                            price = 5;
+                                            break;
+                                        case "Follow":
+                                            price = 10;
+                                            break;
+                                        default:
+                                            price = 10;
+                                            break;
+                                    }
+                                    mListener.onClickGrowPurchase(quantity, type, price);
+                                } else {
+                                    Toast.makeText(getContext(), "Please choose an interaction.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                );
                 break;
             default:
                 break;
         }
-        constraintSet.applyTo(growLayout);
-        connectAccountButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        int quantity;
-                        try {
-                            quantity = Integer.parseInt(quantityET.getText().toString());
-                        } catch (NumberFormatException e) {
-                            Toast.makeText(getContext(), "Please choose a quantity.",
-                                    Toast.LENGTH_SHORT).show();
-                            return;
-                        }
-
-                        int selectId = interactionTypesRG.getCheckedRadioButtonId();
-                        RadioButton selected = interactionTypesRG.findViewById(selectId);
-                        String type = selected.getText().toString();
-                        type = type.substring(0, type.length() - 1);
-                        int price;
-                        switch (type) {
-                            case "Like":
-                                price = 1;
-                                break;
-                            case "Retweet":
-                                price = 5;
-                                break;
-                            default:
-                                price = 10;
-                                break;
-                        }
-
-                        mListener.onClickGrowPurchase(quantity, type, price);
-                    }
-                }
-        );
         return view;
-    }
-
-    private ArrayList<View> getAllViewChildren(View v) {
-        ArrayList<View> result = new ArrayList<>();
-
-        // add the view
-        result.add(v);
-
-        // if the view has children, add them and their children
-        if (v instanceof ViewGroup) {
-            for (int i = 0; i < ((ViewGroup) v).getChildCount(); i++) {
-                result.addAll(getAllViewChildren(((ViewGroup) v).getChildAt(i)));
-            }
-        }
-
-        return result;
     }
 
     @Override
