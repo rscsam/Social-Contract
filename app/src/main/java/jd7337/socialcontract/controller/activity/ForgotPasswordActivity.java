59c1109e97c2a941696273560037d37faad78daf
bbbzhai <movingbob@hotmail.com>
2018-04-13 16:34:14 -0400

    retrieve password

--- app/src/main/java/jd7337/socialcontract/controller/activity/ForgotPasswordActivity.java
+++ app/src/main/java/jd7337/socialcontract/controller/activity/ForgotPasswordActivity.java
@@ -3,20 +3,220 @@ package jd7337.socialcontract.controller.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 import jd7337.socialcontract.R;
+import jd7337.socialcontract.controller.fragment.ProfileFragment;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private EditText emailET;
+    private String email;
+    private String userId;
+    private String salt;
+    private String nonce;
+    private String newPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        emailET = findViewById(R.id.forgot_email);
     }
 
     public void onClickSend(View view) {
         Intent startLogin = new Intent(this, LoginActivity.class);
         startActivity(startLogin);
+        email = emailET.getText().toString();
+        setDefaultPassword();
+        String emailBody = "Your new password is: " + newPass;
+        try {
+            GMailSender sender = new GMailSender("socialContractTest@gmail.com", "TheRipGetRipper");
+            sender.sendMail("Password Reset Request",
+                    emailBody,
+                    "socialContractTest@gmail.com", email);
+        } catch (Exception e) {
+            Log.e("SendMail", e.getMessage(), e);
+        }
+        //
+
     }
+
+    /**
+     * Connect to the change password endpoint on the server to change the user's password in the database
+     * Hashes the password first
+     */
+    public void setDefaultPassword() {
+        //ensures the salt and nonce have been retrieved
+        String hashedPassword;
+        if (checkUser()) {
+            newPass = new String(randomPassword(8));
+            hashedPassword = hashPassword(newPass);
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url = "http://ec2-18-220-246-27.us-east-2.compute.amazonaws.com:3000/changePassword";
+
+            Map<String, String> params = new HashMap<>();
+            params.put("password", hashedPassword);
+            params.put("userId", userId);
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params),
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                boolean success = response.getBoolean("success");
+                                if (success) {
+                                    Toast.makeText(getApplicationContext(),
+                                            "Successfully changed password", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getApplicationContext(),
+                                            response.getString("message"), Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException e) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Error parsing JSON response", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(),
+                                    error.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            ) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> headers = new HashMap<>();
+                    headers.put("Content-Type", "application.json; charset=utf-8");
+                    return headers;
+                }
+            };
+
+            queue.add(jsonObjectRequest);
+        }
+
+
+    }
+
+    /**
+     * Connects to loginInit endpoint to get user's salt and nonce.
+     */
+    private boolean checkUser() {
+        final boolean[] userExist = {false};
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://ec2-18-220-246-27.us-east-2.compute.amazonaws.com:3000/loginInit";
+
+        Map<String, String> params = new HashMap<>();
+        params.put("email", email);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            boolean success = response.getBoolean("success");
+                            if (success) {
+                                userExist[0] = true;
+                                salt = response.getString("salt");
+                                nonce = response.getString("nonce");
+                                userId = response.getString("userId");
+                            } else {
+                                Toast.makeText(getApplicationContext(), response.getString("message"), Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(getApplicationContext(), "Failure parsing JSON", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        return userExist[0];
+    }
+
+
+    private static char[] randomPassword(int len) {
+
+        // A strong password has Cap_chars, Lower_chars,
+        // numeric value and symbols. So we are using all of
+        // them to generate our password
+        String Capital_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String Small_chars = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "0123456789";
+        String symbols = "!@#$%^&*_=+-/.?<>)";
+
+        String values = Capital_chars + Small_chars +
+                numbers + symbols;
+
+        // Using random method
+        Random rndm_method = new Random();
+
+        char[] password = new char[len];
+
+        for (int i = 0; i < len; i++)
+        {
+            // Use of charAt() method : to get character value
+            // Use of nextInt() as it is scanning the value as int
+            password[i] =
+                    values.charAt(rndm_method.nextInt(values.length()));
+
+        }
+        return password;
+    }
+
+    /**
+     * Retrieves the salt and nonce and uses them to hash the password
+     * @param password - password to be hashed
+     * @return String - the hashed password
+     */
+    private String hashPassword(String password) {
+        String concat = password + salt;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(concat.getBytes());
+            byte[] digest = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for (byte b : digest) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+
+            String nextHash = sb.toString() + nonce;
+            MessageDigest md2 = MessageDigest.getInstance("SHA-256");
+            md2.update(nextHash.getBytes());
+            byte[] digest2 = md2.digest();
+            StringBuilder sb2 = new StringBuilder();
+            for (byte b : digest2) {
+                sb2.append(String.format("%02x", b & 0xff));
+            }
+            return sb2.toString();
+        } catch (NoSuchAlgorithmException e) {
+            return concat.substring(0,60);
+        }
+    }
+
 }
